7d76a6a5b76f99fc42e6bbfabd0f17ef1ba998b6
Nont <nontkrub@gmail.com>
2020-11-12 20:12:03 -0600

    Refactor after looking at how others did it
    The type assertion and its companion type switches were really
    interesting.  I also learned a lot about expansion.
    
    I also did use some slick tricks.

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,24 +1,17 @@
 package flatten
 
 func Flatten(item interface{}) (result []interface{}) {
-	result = process(item)
-	n := 0
-	for _, x := range result {
-		if x != nil {
-			result[n] = x
-			n++
-		}
-	}
-	return result[:n]
-}
 
-func process(item interface{}) (result []interface{}) {
-	if items, ok := item.([]interface{}); ok {
-		for _, v := range items {
-			result = append(result, process(v)...)
+	result = []interface{}{}
+
+	switch arr := item.(type) {
+	case []interface{}:
+		for _, v := range arr {
+			result = append(result, Flatten(v)...)
 		}
-		return result
-	} else {
-		return append(result, item)
+	case interface{}:
+		result = append(result, arr)
 	}
+
+	return result
 }
