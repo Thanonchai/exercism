0fbd27888abefc50569dd00cfa9d8d563f8def6f
Nont <nontkrub@gmail.com>
2020-11-20 22:26:22 -0600

    Refactor for conciseness

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -5,15 +5,9 @@ func Triangle(n int) (t [][]int) {
 	t[0] = []int{1}
 	for i := 1; i < n; i++ {
 		arr := make([]int, i+1)
-		for j := 0; j <= i; j++ {
-			left, right := 0, 0
-			if j-1 >= 0 {
-				left = t[i-1][j-1]
-			}
-			if j < i {
-				right = t[i-1][j]
-			}
-			arr[j] = left + right
+		arr[0], arr[i] = 1, 1
+		for j := 1; j < i; j++ {
+			arr[j] = t[i-1][j-1] + t[i-1][j]
 		}
 		t[i] = arr
 	}
