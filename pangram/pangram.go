4685766c82e51e929158f4637f7d473232c73400
Nont <nontkrub@gmail.com>
2021-01-02 17:28:20 -0600

    Refactor based on others' solution

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -2,16 +2,10 @@ package pangram
 
 import "strings"
 
-func IsPangram(input string) bool {
-	pangram := make([]bool, 26)
-	input = strings.ToUpper(input)
-	for _, c := range input {
-		if c >= 'A' && c <= 'Z' {
-			pangram[int(c-'A')] = true
-		}
-	}
-	for _, p := range pangram {
-		if !p {
+func IsPangram(s string) bool {
+	s = strings.ToLower(s)
+	for c := 'a'; c <= 'z'; c++ {
+		if strings.IndexRune(s, c) == -1 {
 			return false
 		}
 	}
